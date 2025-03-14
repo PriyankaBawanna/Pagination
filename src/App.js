7b340bde2a57c971efc95225235a167c449fdce7
Priyanka Bawanna <priyankabawanna@Priyankas-MacBook-Air.local>
2025-03-14 17:36:23 +0530

    paggination

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import EmployeeTable from "./components/EmployeeTable";
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1>Employee Data Table</h1>
+      <EmployeeTable></EmployeeTable>
+    </>
+  );
+}
+
+export default App;
